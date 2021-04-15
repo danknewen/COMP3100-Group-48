0ad06aebaced189191bbb13763cad16c82b4f226
SneakyDaOne <63494096+SneakyDaOne@users.noreply.github.com>
2021-04-15 23:27:30 +1000

    Add files via upload
    Small Storage class for storage of server information

---
+++ Storage.java
@@ -0,0 +1,23 @@
+public class Storage{
+    public String ID;
+    public int type;
+    public String Status;
+    public int BootupTime;
+    public int core;
+    public int memory;
+    public int disk;
+    public int wJobs;
+    public int rJobs;
+
+    public Storage(){
+        ID = "";
+        type = 0;
+        Status = "";
+        BootupTime = 0;
+        core = 0;
+        memory = 0;
+        disk = 0;
+        wJobs = 0;
+        rJobs = 0;
+    }
+}
